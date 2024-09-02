933191b42d0c7e146ce93c5b3ce80a44566f823a
zahid-akhtar7979 <mycryptotelegram@gmail.com>
2024-09-02 19:20:02 +0530

    Submitted merge sorted array leetcode problem

--- src/main/java/org/dsa/Main.java
+++ src/main/java/org/dsa/Main.java
@@ -1,7 +1,12 @@
 package org.dsa;
 
+import org.dsa.array_string.MergeSortedArray;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int []nums1 = {3,4,5,6,0,0};
+        int []nums2 = {1,2};
+        mergeSortedArray.merge(nums1,4,nums2,2);
     }
 }
